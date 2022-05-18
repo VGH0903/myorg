e3cf2c0d44cad64812f2879dcd8f7c1851168af9
TungYueh <tungyuehwu@vghtc.gov.tw>
2022-05-18 11:07:17 +0800

    init

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '@myorg/data'
+
+//interface Todo {
+//  title: string;
+//}
+
+@Component({
+  selector: 'myorg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  //todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient){
+    this.fetch();
+  }
+
+  fetch(){
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    //this.todos.push({
+    //  title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    //});
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
+}
